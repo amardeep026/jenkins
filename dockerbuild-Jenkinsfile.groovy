5766c0210fb5404b6cf2c77b1471671d289cff86
amardeep026 <45689520+amardeep026@users.noreply.github.com>
2024-10-19 23:05:43 +0530

    Add files via upload

---
+++ dockerbuild-Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline{
+    agent any 
+    stages{
+        stage("git pull"){
+            steps{
+                git url: "https://github.com/LondheShubham153/django-notes-app.git", branch: "main"
+            }
+        }
+        stage("DOcker build"){
+            steps{
+                sh "docker build . -t django:latest"
+            }
+        }
+        stage ("Deploy"){
+            steps{
+                sh "docker run -d -p 8000:8000 --name django django:latest "
+            }
+        }
+        
+    }
+}
